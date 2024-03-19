5c0b303694083e12efe56a19ceac2471df0f59eb
leedongkyu <leedong0225@icloud.com>
2024-03-20 00:08:45 +0900

    feat login

---
+++ mission1/scripts/switch.js
@@ -0,0 +1,13 @@
+let flag = false;
+      function toggleSwitch() {
+        const passIcon = document.getElementById('passIcon');
+        const switchElement = document.getElementById("password");
+        const passBox = document.getElementById("shown");
+        flag=!flag;
+        if (flag) {
+          passIcon.src="/icon/openPassword.png"
+          return (passBox.type = "text");
+        }
+        passIcon.src="/icon/hidePassword.png"
+        return passBox.type = "password";
+      }
