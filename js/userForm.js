6aeee41f272601e9fea17cc6d1af0b711239b7ed
haze <skdml20022@gmail.com>
2024-03-20 17:07:35 +0900

    refactor: toggleClassName 함수 하나로 통일

--- js/userForm.js
+++ js/userForm.js
@@ -2,12 +2,9 @@ const pwdBtnElement = document.getElementById("passwordBtn");
 const checkPwdBtnElement = document.getElementById("checkPasswordBtn");
 
 // 비밀번호를 보여주는 버튼에 아이콘 변경 클래스 추가
-function toggleClassName1() {
-    pwdBtnElement.classList.toggle("visibility");
-}
-function toggleClassName2() {
-    checkPwdBtnElement.classList.toggle("visibility");
+function toggleClassName(event) {
+    event.target.classList.toggle("visibility");
 }
 
-pwdBtnElement.addEventListener("click", toggleClassName1);
-checkPwdBtnElement.addEventListener("click", toggleClassName2);
+pwdBtnElement.addEventListener("click", toggleClassName);
+checkPwdBtnElement.addEventListener("click", toggleClassName);
