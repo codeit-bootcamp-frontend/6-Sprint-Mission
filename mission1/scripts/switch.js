e6ed5fe0a0851a2c217b529676afaff7e6464aff
leedongkyu <leedong0225@icloud.com>
2024-03-21 15:57:53 +0900

    feat 회원가입페이지 구현

--- mission1/scripts/switch.js
+++ mission1/scripts/switch.js
@@ -1,13 +1,14 @@
 let flag = false;
-      function toggleSwitch() {
-        const passIcon = document.getElementById('passIcon');
-        const switchElement = document.getElementById("password");
-        const passBox = document.getElementById("shown");
-        flag=!flag;
-        if (flag) {
-          passIcon.src="/icon/openPassword.png"
-          return (passBox.type = "text");
-        }
-        passIcon.src="/icon/hidePassword.png"
-        return passBox.type = "password";
-      }
+function toggleSwitch() {
+  const passIcon = document.getElementById("passIcon");
+  const switchElement = document.getElementById("password");
+  const passBox = document.getElementById("shown");
+  flag = !flag;
+  if (flag) {
+    passIcon.src = "/icon/openPassword.png";
+    return (passBox.type = "text");
+  }
+  passIcon.src = "/icon/hidePassword.png";
+  return (passBox.type = "password");
+}
+
