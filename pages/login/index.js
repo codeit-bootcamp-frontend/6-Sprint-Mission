8a500b71541853de4f6d69d212f1a1d503229095
Sombian <sombian.software@gmail.com>
2024-03-22 11:09:38 +0900

    refactor: import hierarchy

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,1 +1,20 @@
-import "/pages/signup/index.js";
+for (const element of document.querySelectorAll(`form > .field > .wrapper:has(input[type="password"])`))
+{
+	const [input, img] = [element.querySelector(`input`), element.querySelector(`img`)];
+
+	img.addEventListener(`click`, (event) => {
+		switch (input.type)
+		{
+			case `password`:
+			{
+				input.type = `text`; img.src = `/assets/icons/visible.svg`;
+				break;
+			}
+			default:
+			{
+				input.type = `password`; img.src = `/assets/icons/invisible.svg`;
+				break;
+			}
+		}
+	});
+}
--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,40 +1,18 @@
+import "/pages/login/index.js";
+
 const [password, password_confirm] = [document.querySelector(`#password`), document.querySelector(`#password-confirm`)];
 
-if (password !== null && password_confirm !== null)
+function validator()
 {
-	function validator()
-	{
-		password_confirm.setCustomValidity(password.value === password_confirm.value ? `` : `비밀번호 불일치`);
-	}
-
-	for (const element of [password, password_confirm])
-	{
-		element.addEventListener(`change`, (event) => {
-			validator();
-		});
-		element.addEventListener(`keyup`, (event) => {
-			validator();
-		});
-	}
+	password_confirm.setCustomValidity(password.value === password_confirm.value ? `` : `비밀번호 불일치`);
 }
 
-for (const element of document.querySelectorAll(`form > .field > .wrapper:has(input[type="password"])`))
+for (const element of [password, password_confirm])
 {
-	const [input, img] = [element.querySelector(`input`), element.querySelector(`img`)];
-
-	img.addEventListener(`click`, (event) => {
-		switch (input.type)
-		{
-			case `password`:
-			{
-				input.type = `text`; img.src = `/assets/icons/visible.svg`;
-				break;
-			}
-			default:
-			{
-				input.type = `password`; img.src = `/assets/icons/invisible.svg`;
-				break;
-			}
-		}
+	element.addEventListener(`change`, (event) => {
+		validator();
+	});
+	element.addEventListener(`keyup`, (event) => {
+		validator();
 	});
 }
