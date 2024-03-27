0913ae090f591b520a4f016d21ae5e42959d5252
Hyunji <hjcho2303@naver.com>
2024-03-27 15:28:45 +0900

    ♻️ Modify function password toggle btn

--- signin/passwordToggle.js
+++ signin/passwordToggle.js
@@ -1,11 +1,14 @@
 // 비밀번호 타입 토글버튼 구현
 const eyeBtn = document.querySelector('.eye_btn');
 const pwInput = document.querySelector('#password');
+const pwcheckInput = document.querySelector('#password-check');
 
-eyeBtn.addEventListener('click', function togglePassword() {
+function togglePassword() {
     if (pwInput.type !== 'text') {
         pwInput.type = 'text';
     } else {
         pwInput.type = 'password';
     }
-});
+}
+
+eyeBtn.addEventListener('click', togglePassword);
