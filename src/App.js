78edd6fa2fcac0f1c787fe018f5540eedcd6d1db
emotigom <ahnsangkyoon@gmail.com>
2024-04-12 23:25:29 +0900

    fix: changing fetch function of app.js. feat: added top nav bar.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
+const baseURL = "https://panda-market-api.vercel.app"
+const page = 1;
+const pageSize = 10;
+
 
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+
+useEffect(() => {
+  fetch(`${baseURL}/products?page=${page}&pageSize=${pageSize}`)
+    .then((res) => res.json())
+    .then((data) => {
+      setProducts(data.list);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.images} />
+          <h2>{product.name}</h2>
+          <p>{product.price}</p>
+        </div>
+      ))};
     </div>
   );
 }
