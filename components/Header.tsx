9437057b121f54dc3bf8490d45af68b6e71ee14b
haze <skdml20022@gmail.com>
2024-05-30 23:09:37 +0900

    feat: header component

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import styles from './Header.module.css';
+import { useIsMobile } from '@/hooks/useIsMobile';
+import Image from 'next/image';
+import logo from '@/public/logo.png';
+import mobileLogo from '@/public/logo-mobile.png';
+import profileIcon from '@/public/icon/profile.png';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.container}>
+        <div className={styles.menus}>
+          <div className={styles.logo}>
+            <Link href="/">
+              <Image
+                fill
+                src={useIsMobile() ? mobileLogo : logo}
+                alt="판다마켓 로고"
+              />
+            </Link>
+          </div>
+          <Link href="/board">자유게시판</Link>
+          <Link href="/market">중고마켓</Link>
+        </div>
+        <div className={styles.icon}>
+          <Link href="">
+            <Image fill src={profileIcon} alt="마이페이지로 이동" />
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>판다마켓</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
